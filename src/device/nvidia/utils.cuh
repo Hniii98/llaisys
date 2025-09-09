362f80e766e1c93452de2a9514cb8845a81a2c4f
Hniii <752608928@qq.com>
2025-09-09 15:04:41 +0800

    fix: fix a wrong naming

---
+++ src/device/nvidia/utils.cuh
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string>
+#include <cuda_runtime.h>
+#include <system_error>
+
+
+namespace llaisys::device::nvidia::utils {
+
+
+inline const std::error_category &cudaErrorCategory() noexcept {
+    // 单例
+    static struct : std::error_category {
+        char const *name() const noexcept override {
+            return "cuda";
+        }
+
+        std::string message(int ev) const override {
+            return cudaGetErrorString(static_cast<cudaError_t>(ev));
+        }
+    } category;
+
+    return category;
+}
+
+
+inline std::error_code makeCudaErrorCode(cudaError_t e) noexcept {
+    return std::error_code(static_cast<int>(e), cudaErrorCategory());
+}
+
+
+inline void throwCudaError(cudaError_t err, const char *file, int line) {
+    throw std::system_error(makeCudaErrorCode(err),
+                            std::string(file ? file : "??") + ":" + std::to_string(line));
+}
+
+
+
+
+
+} // namespace llaisys::device::nvidia::utils
+
+
+#define CHECK_CUDA(expr) \
+    do { \
+        cudaError_t err__ = (expr); \
+        if (err__ != cudaSuccess)  { \
+            ::llaisys::device::nvidia::utils::throwCudaError(err__, __FILE__, __LINE__); \
+        } \
+    } while (0)
