d1e126c1f9a112626a88174de8863779ceca71cf
Hniii <752608928@qq.com>
2025-10-09 21:43:05 +0800

    feat: support check cudnn marco

--- src/device/nvidia/utils.cuh
+++ src/device/nvidia/utils.cuh
@@ -4,14 +4,21 @@
 #include <cuda_runtime.h>
 #include <system_error>
 #include <limits>
+#include <type_traits>  
+
+#include <cuda_fp16.h>          
+#include <cuda_bf16.h>          
 
 #include <cublas_v2.h>   
+#include <cudnn.h>
+#include <cudnn_frontend.h> 
+
 
 
 
 namespace llaisys::device::nvidia::utils {
 
-
+/*----------------CUDA----------------*/
 inline const std::error_category &cudaErrorCategory() noexcept {
     // 单例
     static struct : std::error_category {
@@ -38,7 +45,7 @@ inline void throwCudaError(cudaError_t err, const char *file, int line) {
                             std::string(file ? file : "??") + ":" + std::to_string(line));
 }
 
-
+/*----------------cuBLAS----------------*/
 inline const std::error_category& cublasErrorCategory() noexcept {
     static struct : std::error_category {
         const char* name() const noexcept override { return "cublas"; }
@@ -56,8 +63,31 @@ inline std::error_code makeCublasErrorCode(cublasStatus_t s) noexcept {
 inline void throwCublasError(cublasStatus_t s, const char* file, int line) {
     throw std::system_error(makeCublasErrorCode(s),
                             std::string(file ? file : "??") + ":" + std::to_string(line));
+
 }
 
+/*----------------cuDNN----------------*/
+inline const std::error_category& cudnnErrorCategory() noexcept {
+    static struct : std::error_category {
+        const char* name() const noexcept override { return "cudnn"; }
+        std::string message(int ev) const override {
+            return cudnnGetErrorString(static_cast<cudnnStatus_t>(ev));
+        }
+    } category;
+    return category;
+}
+
+inline std::error_code makeCudnnErrorCode(cudnnStatus_t s) noexcept {
+    return std::error_code(static_cast<int>(s), cudnnErrorCategory());
+}
+
+inline void throwCudnnError(cudnnStatus_t s, const char* file, int line) {
+    throw std::system_error(makeCudnnErrorCode(s),
+                            std::string(file ? file : "??") + ":" + std::to_string(line));
+}
+
+
+/*----------------helper--------------*/
 template<typename T>
 __device__ __host__ float load_as_float(const T* p) {
     if constexpr (std::is_same_v<T, __half>)        return __half2float(*p);
@@ -71,9 +101,6 @@ __device__ __host__ inline void store_from_float(T* p, float v) {
     if constexpr (std::is_same_v<T, __nv_bfloat16>) { *p = __float2bfloat16(v); return; }
     *p = v; // float
 }
-
-
-
 
 
 } // namespace llaisys::device::nvidia::utils
@@ -94,6 +121,17 @@ __device__ __host__ inline void store_from_float(T* p, float v) {
             ::llaisys::device::nvidia::utils::throwCublasError(st__, __FILE__, __LINE__); \
         } \
     } while (0)
+
+#define CHECK_CUDNN(expr) \
+    do { \
+        cudnnStatus_t st__ = (expr); \
+        if (st__ != CUDNN_STATUS_SUCCESS) { \
+            ::llaisys::device::nvidia::utils::throwCudnnError(st__, __FILE__, __LINE__); \
+        } \
+    } while(0)
+
+
+
 
 
 inline unsigned int safe_grid_size(size_t n, unsigned int block_size) {
